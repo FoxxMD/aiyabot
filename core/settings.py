88489e84ec65924ff7b36d474a21ea1d0293eb9d
FoxxMD <FoxxMD@users.noreply.github.com>
2024-02-05 09:26:11 -0500

    fix: Fix change in model name key
    (cherry picked from commit c08b5c7f72dcdcd4b648097227a6af83e558314a)

--- core/settings.py
+++ core/settings.py
@@ -552,9 +552,12 @@ def populate_global_vars():
             for model in r.json():
                 norm_csv_path = os.path.normpath(row[1])
                 norm_api_path = os.path.normpath(model['filename'])
+                name = model.get('name')
+                if name is None:
+                    name = model.get('model_name')
                 if norm_csv_path.split(os.sep)[-1] == norm_api_path.split(os.sep)[-1] \
-                        or norm_csv_path.replace(os.sep, '_') == model['name']:
-                    global_var.model_info[row[0]] = model['title'], model['name'], model['hash'], row[2]
+                        or norm_csv_path.replace(os.sep, '_') == name:
+                    global_var.model_info[row[0]] = model['title'], name, model['hash'], row[2]
                     break
     # add "Default" if models.csv is on default, or if no model matches are found
     if not global_var.model_info:
